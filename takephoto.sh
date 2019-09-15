1eb0956a70a5c16512892c783f20b2a4426e42be
alice <ojamarushealice@yahoo.com>
2019-09-14 20:39:37 -0700

    Add comments

--- takephoto.sh
+++ takephoto.sh
@@ -3,5 +3,8 @@ SHARE_DIR=/home/alice/test
 TIMESTAMP=`date +%s`
 FILENAME="$TIMESTAMP.jpg"
 
+# take photo
 raspistill -n -o - |  convert -rotate 90 jpg:- "$BASE_DIR/$FILENAME"
+
+# copy it into the share dir
 cp "$BASE_DIR/$FILENAME" "$SHARE_DIR/$FILENAME"
